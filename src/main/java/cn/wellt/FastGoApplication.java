ba98fecfbc4ddf6034684c7a9ff8556ddac0bb82
caojingchen <42629624@qq.com>
2018-04-19 14:58:25 +0800

    create the project

---
+++ src/main/java/cn/wellt/FastGoApplication.java
@@ -0,0 +1,12 @@
+package cn.wellt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastGoApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/wellt/FastgoApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.wellt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastgoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
